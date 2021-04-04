7d7256979e7a707b5c05dbf3f0936b8e0566191f
Zeb Piasecki <zeb@zebulon.dev>
2021-04-04 15:51:09 -0400

    Add downloaded message to indicate progress

--- src/download.rs
+++ src/download.rs
@@ -1,5 +1,6 @@
-use std::path::Path;
+use std::{path::Path, time::Instant};
 
+use colored::*;
 use futures::StreamExt;
 use reqwest::Url;
 use tokio::{fs::File, io::AsyncWriteExt};
@@ -8,12 +9,29 @@ use crate::{context::Context, error::Error};
 
 pub async fn download(ctx: &Context, link: Url, path: &Path) -> Result<(), Error> {
     let mut file = File::create(path).await?;
-    let mut stream = ctx.client.get(link).send().await?.bytes_stream();
+    let mut stream = ctx.client.get(link.clone()).send().await?.bytes_stream();
+
+    let then = Instant::now();
+    let mut total_bytes = 0;
 
     while let Some(item) = stream.next().await {
         let bytes = item?;
         file.write(&bytes).await?;
+        total_bytes += bytes.len();
     }
+
+    let duration = Instant::now().duration_since(then);
+
+    let bytes_per_second = total_bytes as f64 / duration.as_secs_f64();
+    let bytes_per_second = bytes_per_second.round() as u64;
+
+    println!(
+        "Downloaded {} totaling {} bytes at {}{}",
+        &link.to_string().green(),
+        bytesize::to_string(total_bytes as u64, false).green(),
+        bytesize::to_string(bytes_per_second, false).green(),
+        "/s".green()
+    );
 
     Ok(())
 }
